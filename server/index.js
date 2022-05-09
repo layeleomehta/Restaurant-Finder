51209a669f56d5195e0ca1c282b89767edd652c6
Laye Mehta <layeamehta@gmail.com>
2022-05-09 17:19:51 -0600

    added structure for all api endpoints

--- server/index.js
+++ server/index.js
@@ -3,6 +3,33 @@ const express = require("express");
 const app = express(); 
 const port = process.env.PORT || 3001; 
 
+// routes
+// retrieve all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+    res.json("Get route for all restaurants")
+}); 
+
+// retrieve restaurant with specified id
+app.get("/api/v1/restaurants/:id", (req, res) => {
+    res.json(`Get route for particular restaurant with id: ${req.params.id}`); 
+}); 
+
+// create a restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+    res.json("Create restaurants route")
+}); 
+
+// update a restaurant with specified id
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    res.json(`Update route for particular restaurant with id: ${req.params.id}`); 
+}); 
+
+// delete a restaurant with specified id
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.json(`Delete route for particular restaurant with id: ${req.params.id}`); 
+}); 
+
+// starting up server
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`); 
 })
