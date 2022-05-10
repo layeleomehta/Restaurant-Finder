3c829dfed4d510de5b820c521f34d20568e52178
Laye Mehta <layeamehta@gmail.com>
2022-05-10 13:22:07 -0600

    finished CRUD routes for restaurants table in db

--- server/index.js
+++ server/index.js
@@ -35,23 +35,59 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
         })
     } catch (err) {
         console.log(err.message); 
-        
     }
 }); 
 
 // create a restaurant
-app.post("/api/v1/restaurants", (req, res) => {
-    res.json("Create restaurants route")
+app.post("/api/v1/restaurants", async (req, res) => {
+    try {
+        const result = await db.query("INSERT INTO restaurants(name, location, price_range) VALUES ($1, $2, $3) RETURNING *", [
+            req.body.name, 
+            req.body.location, 
+            req.body.price_range
+        ]); 
+
+        res.status(200).json({
+            status: "success", 
+            restaurantData: result.rows[0]
+        })
+    } catch (err) {
+        console.log(err.message); 
+    }
 }); 
 
 // update a restaurant with specified id
-app.put("/api/v1/restaurants/:id", (req, res) => {
-    res.json(`Update route for particular restaurant with id: ${req.params.id}`); 
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+    try {
+        const result = await db.query("UPDATE restaurants SET name=$1, location=$2, price_range=$3  WHERE id=$4 RETURNING *", [
+            req.body.name, 
+            req.body.location, 
+            req.body.price_range, 
+            req.params.id
+        ]); 
+
+        res.status(200).json({
+            status: "success", 
+            restaurantData: result.rows[0]
+        })
+    } catch (err) {
+        console.log(err.message); 
+    }
 }); 
 
 // delete a restaurant with specified id
-app.delete("/api/v1/restaurants/:id", (req, res) => {
-    res.json(`Delete route for particular restaurant with id: ${req.params.id}`); 
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+    try {
+        const result = await db.query("DELETE FROM restaurants WHERE id=$1", [
+            req.params.id
+        ]); 
+
+        res.status(200).json({
+            status: "success"
+        })
+    } catch (err) {
+        console.log(err.message); 
+    }
 }); 
 
 // starting up server
